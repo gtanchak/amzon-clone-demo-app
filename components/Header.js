d378b861cdaa5d969cc0fb34ace83a997cd6bdd7
ghanshyamtanchak <ghtanchak77@gmail.com>
2023-02-18 22:23:39 +0530

    completed top header

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,15 @@
 import Image from "next/image";
-import { SearchIcon, ShoppingCartIcon } from "@heroicons/react/outline";
+import {
+  MenuIcon,
+  SearchIcon,
+  ShoppingCartIcon,
+} from "@heroicons/react/outline";
 
 const Header = () => {
   return (
     <header>
       {/* Top Nav */}
-      <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
+      <div className="flex items-center bg-amazon_blue p-1 pl-6 flex-grow py-2">
         <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
           <Image
             src="/amazon_logo.png"
@@ -17,7 +21,7 @@ const Header = () => {
           />
         </div>
         {/* Search */}
-        <div className="bg-yellow-400 hover:bg-yellow-500 hidden sm:flex items-center h-10 rounded-md flex-grow cursor-pointer">
+        <div className="bg-yellow-400 hover:bg-yellow-500 hidden sm:flex items-center h-10 rounded-md flex-grow cursor-pointer mx-6">
           <input
             type="text"
             className="p-2 h-full flex-grow w-6 rounded-l-md focus:outline-none flex-shrink"
@@ -26,23 +30,34 @@ const Header = () => {
         </div>
 
         {/* Account Info */}
-        <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap">
+        <div className="text-white flex items-center text-xs space-x-6 whitespace-nowrap mr-6">
           <div className="link">
             <p>Hello Ghanshyam Tanchak</p>
-            <p>Account & Lists</p>
+            <p className="font-extrabold md:text-sm">Account & Lists</p>
           </div>
           <div className="link">
             <p>Returns</p>
-            <p>& Orders</p>
+            <p className="font-extrabold md:text-sm">& Orders</p>
           </div>
-          <div className="link">
-            <ShoppingCartIcon />
-            <p>Basket</p>
+          <div className="relative link flex items-center">
+            <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">
+              0
+            </span>
+            <ShoppingCartIcon className="h-10" />
+            <p className="hidden md:inline font-extrabold md:text-sm mt-2">
+              Basket
+            </p>
           </div>
         </div>
       </div>
       {/* Bottom Nav */}
-      <div></div>
+      <div className="flex items-center bg-amazon_blue-light space-x-3 p-2 pl-6 text-white">
+        <p className="link flex items-center">
+          <MenuIcon className="h-6 mr-1" />
+          All
+        </p>
+        <p className="link">Prime Video</p>
+      </div>
     </header>
   );
 };
