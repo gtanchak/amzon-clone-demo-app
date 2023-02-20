959d9c7d979da72edcf77274dca14317a4278cde
ghanshyamtanchak <ghtanchak77@gmail.com>
2023-02-20 21:25:12 +0530

    added slider component

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,17 +4,16 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
- 
   ],
   theme: {
     extend: {
-      colors:{
-        amazon_blue:{
+      colors: {
+        amazon_blue: {
           light: "#232F3E",
           DEFAULT: "#131921",
-        }
-      }
+        },
+      },
     },
   },
   plugins: [],
-}
+};
