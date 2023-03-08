aeff0cca5acce9fa5d90336782739cc35f59d402
ghanshyamtanchak <ghtanchak77@gmail.com>
2023-03-08 16:23:47 +0530

    added product list

---
+++ src/components/Banner.tsx
@@ -0,0 +1,43 @@
+import { FC } from "react";
+import { Carousel } from "react-responsive-carousel";
+
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+const bannerImages = [
+  {
+    src: "/banner_img_1.jpg",
+    alt: "banner-1",
+  },
+  {
+    src: "/banner_img_2.jpg",
+    alt: "banner-2",
+  },
+  {
+    src: "/banner_img_3.jpg",
+    alt: "banner-3",
+  },
+];
+
+const Banner: FC = () => {
+  return (
+    <div className="relative">
+      <div className="absolute w-full h-32 bg-gradient-to-t from-gray-100 to-transparent bottom-0 z-20" />
+      <Carousel
+        autoPlay
+        infiniteLoop
+        showStatus={false}
+        showIndicators={false}
+        showThumbs={false}
+        interval={5000}
+      >
+        {bannerImages.map(({ src, alt }) => (
+          <div key={alt}>
+            <img loading="lazy" src={src} alt={alt} />
+          </div>
+        ))}
+      </Carousel>
+    </div>
+  );
+};
+
+export default Banner;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,41 @@
+import { NextPage, NextPageContext } from "next";
 import Head from "next/head";
+import Banner from "../components/Banner";
+import Header from "../components/Header";
+import ProductFeed from "../components/ProductFeed";
+import { IProduct } from "../interface";
 
-const Home = () => {
+interface Props {
+  products: IProduct[];
+}
+
+const Home: NextPage<Props> = ({ products }) => {
   return (
-    <div>
+    <div className="bg-gray-100">
       <Head>
         <title>Amazon Clone</title>
       </Head>
+
+      {/* HEADER */}
+      <Header />
+      <main className="max-w-screen-2xl mx-auto ">
+        <Banner />
+        <ProductFeed products={products} />
+      </main>
     </div>
   );
 };
 
 export default Home;
+
+export const getServerSideProps = async (context: NextPageContext) => {
+  const products = await fetch("https://fakestoreapi.com/products")
+    .then((res) => res.json())
+    .catch((err) => console.log(err));
+
+  return {
+    props: {
+      products,
+    },
+  };
+};
